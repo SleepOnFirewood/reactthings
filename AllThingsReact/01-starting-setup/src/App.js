68f70418932602d6dad9dab5e870842d412dd35a
jellomanforever <julianmaleur123@gmail.com>
2023-07-22 16:40:32 -0400

    add concept file

--- AllThingsReact/01-starting-setup/src/App.js
+++ AllThingsReact/01-starting-setup/src/App.js
@@ -2,7 +2,8 @@
 import componentsImage from './assets/images/components.png';
 import stateImage from './assets/images/state.png';
 import eventsImage from './assets/images/events.png';
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
+import Concept from './components/Concept/Concept';
 
 const concepts = [
   {
@@ -30,21 +31,18 @@ function App() {
     <div>
       <Header></Header>
       <ul id="concepts">
-        <li className="concept">
-          <img src={concepts[0].image} alt={concepts[0].title} />
-          <h2>{concepts[0].title}</h2>
-          <p>{concepts[0].description}</p>
-        </li>
-        <li className="concept">
-          <img src={concepts[1].image} alt={concepts[1].title} />
-          <h2>{concepts[1].title}</h2>
-          <p>{concepts[1].description}</p>
-        </li>
-        <li className="concept">
-          <img src={concepts[2].image} alt={concepts[2].title} />
-          <h2>{concepts[2].title}</h2>
-          <p>{concepts[2].description}</p>
-        </li>
+        <Concept 
+        image = {concepts[0].image} 
+        title={concepts[0].title} 
+        description = {concepts[0].description}></Concept>
+         <Concept 
+        image = {concepts[1].image} 
+        title={concepts[1].title} 
+        description = {concepts[1].description}></Concept>
+         <Concept 
+        image = {concepts[2].image} 
+        title={concepts[2].title} 
+        description = {concepts[2].description}></Concept>
       </ul>
       
     </div>
