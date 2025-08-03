143f1dc0f293b14a5e0996779810ae3b9211cc4b
SleepOnFirewood <50755859+SleepOnFirewood@users.noreply.github.com>
2025-08-02 21:36:53 -0400

    fixing flex

--- BubbleTeaMenu/main/myapp/src/App.js
+++ BubbleTeaMenu/main/myapp/src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import Border from './component/Border/borderFrame';
 import BubbleTeaContainer from './component/BubbleTeaContainer/BubbleTeaContainer';
-import Card from './component/Card/Card';
 import Title from './component/Title/Title'
 import Top10Container from './component/Top10Container/Top10Container';
 
@@ -12,17 +11,12 @@ function App() {
       <Border>
 
         <Top10Container>
-          <Title
+          {/* <Title
           title ="Top 10 Favourites">
-          </Title>
-          {/* <Card 
-            title="Wintermelon Basil" 
-            description="Fresh winter melon puree blended with basil leaves."
-            price="$5.99"
-          /> */}
-          <BubbleTeaContainer>
-            
-          </BubbleTeaContainer>
+          </Title> */}
+          {/* <BubbleTeaContainer>
+
+          </BubbleTeaContainer> */}
         </Top10Container>
       </Border>
 
--- BubbleTeaMenu/main/myapp/src/component/BubbleTeaContainer/BubbleTeaContainer.js
+++ BubbleTeaMenu/main/myapp/src/component/BubbleTeaContainer/BubbleTeaContainer.js
@@ -6,60 +6,6 @@ function BubbleTeaContainer(props)
 {
     return(
         <div className='BubbleTeaContainer'>
-            <div className='top-cards'>
-            <Card 
-            title="Wintermelon Basil" 
-            description="Fresh winter melon puree blended with basil leaves."
-            price="$5.99"
-            image='/images/boba.png'/>
-            <Card 
-            title="Wintermelon Basil" 
-            description="Fresh winter melon puree blended with basil leaves."
-            price="$5.99"
-            image='/images/boba.png'/>
-            <Card 
-            title="Wintermelon Basil" 
-            description="Fresh winter melon puree blended with basil leaves."
-            price="$5.99"
-            image='/images/boba.png'/>
-            <Card 
-            title="Wintermelon Basil" 
-            description="Fresh winter melon puree blended with basil leaves."
-            price="$5.99"
-            image='/images/boba.png'/>
-            <Card 
-            title="Wintermelon Basil" 
-            description="Fresh winter melon puree blended with basil leaves."
-            price="$5.99"
-            image='/images/boba.png'/>
-            </div>
-            <div className='bottom-cards'>
-            <Card 
-            title="Wintermelon Basil" 
-            description="Fresh winter melon puree blended with basil leaves."
-            price="$5.99"
-            image='/images/boba.png'/>
-            <Card 
-            title="Wintermelon Basil" 
-            description="Fresh winter melon puree blended with basil leaves."
-            price="$5.99"
-            image='/images/boba.png'/>
-            <Card 
-            title="Wintermelon Basil" 
-            description="Fresh winter melon puree blended with basil leaves."
-            price="$5.99"
-            image='/images/boba.png'/>
-            <Card 
-            title="Wintermelon Basil" 
-            description="Fresh winter melon puree blended with basil leaves."
-            price="$5.99"
-            image='/images/boba.png'/>
-            <Card 
-            title="Wintermelon Basil" 
-            description="Fresh winter melon puree blended with basil leaves."
-            price="$5.99"
-            image='/images/boba.png'/>
-            </div>
         </div>
     )
 
