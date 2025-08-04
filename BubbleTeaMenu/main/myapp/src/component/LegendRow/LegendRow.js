bad9913579164151da14af025c0897c6c5418d57
SleepOnFirewood <50755859+SleepOnFirewood@users.noreply.github.com>
2025-08-04 15:58:09 -0400

    adjust styles for title

--- BubbleTeaMenu/main/myapp/src/component/LegendRow/LegendRow.js
+++ BubbleTeaMenu/main/myapp/src/component/LegendRow/LegendRow.js
@@ -7,11 +7,11 @@ function LegendRow(props)
     return(
         <div className='LegendRow-Frame'>
             <div className="legend-item">
-                <FaSnowflake size={50} className="snowflake-blue"/>Cold</div>
+                <FaSnowflake size={40} className="snowflake-blue"/>Cold</div>
             <div className="legend-item">
-                <FaFire size={50} className="fire-red"/>Hot</div>
+                <FaFire size={40} className="fire-red"/>Hot</div>
             <div className="legend-item">
-                <FaLeaf size={50} className="nonDairy"/>Non-Dairy</div>            
+                <FaLeaf size={40} className="nonDairy"/>Non-Dairy</div>            
         </div>
     );
 
