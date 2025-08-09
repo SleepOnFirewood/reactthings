670cd63e05968ce00793e7709bc7b93da8cbdfd8
SleepOnFirewood <50755859+SleepOnFirewood@users.noreply.github.com>
2025-08-09 09:48:02 -0400

    add footer

--- BubbleTeaMenu/main/myapp/src/Pages/SecondPage.js
+++ BubbleTeaMenu/main/myapp/src/Pages/SecondPage.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import Border from '../component/Border/borderFrame';
+import BubbleTeaContainer from '../component/BubbleTeaContainer/BubbleTeaContainer';
+import Title from '../component/Title/Title';
+import Top10Container from '../component/Top10Container/Top10Container';
+import TopCards from '../component/TopCards/TopCards';
+import Card from '../component/Card/Card';
+import BottomCards from '../component/BottomCards/BottomCards';
+import LegendRow from '../component/LegendRow/LegendRow';
+import Logo from '../component/Logo/Logo';
+import LogoTitle from '../component/LogoTitle/LogoTitle';
+import CardList from '../component/CardList/CardList';
+import Ads from '../component/Ads/Ads';
+import DisplayContainer from '../component/DisplayContainer/DisplayContainer';
+import Footer from '../component/Footer/Footer';
 
 function SecondPage() {
     return (
-        <div>
-            <h1>Welcome to Another Page!</h1>
-            <p>This is the content for another page in your React application.</p>
-        </div>
+      <Border>
+        <Logo image='/images/bubbleteabrewerlogo.png'/>
+        <LogoTitle>BubbleTea Brewers</LogoTitle>
+        <Top10Container>
+        </Top10Container>
+        <Footer></Footer>
+      </Border>
     );
 }
 
---
+++ BubbleTeaMenu/main/myapp/src/component/Footer/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer(props)
+{
+    return (
+        <div className="bubble-tea-section">
+            <div className="options-container">
+                {/* Toppings Section */}
+                <div className="option-section">
+                    <h3>Toppings</h3>
+                    <ul className="toppings-list">
+                        <li>Mini Mochi</li>
+                        <li>Red Bean</li>
+                        <li>Pudding</li>
+                        <li>Lychee Jelly</li>
+                        <li>Mango Jelly Star</li>
+                        <li>Tapioca</li>
+                        <li>Cheese Foam</li>
+                        <li>Rainbow Jelly</li>
+                        <li>Coffee Jelly</li>
+                        <li>Aoe Vera</li>
+                        <li>Mango Popping Pearl</li>
+                        <li>Cyrstal Jelly Boba</li>
+                        <li>Peach</li>
+                        <li>Sago Pearl</li>
+                        <li>Green Apple Jelly</li>
+                        <li>Green Apple Popping Pearl</li>
+                        <li>Yogurt Popping Pearl</li>
+                        <li>Strawberry Popping Peral</li>
+                        <li>Lychee Popping Pearl</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Footer;
